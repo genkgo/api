7841a65619450e9faf63ba309afb2c67b1606088
Frederik Bosch <f.bosch@genkgo.nl>
2022-12-06 10:21:31 +0100

    improve readme

--- examples/example.create.and.delete.person.php
+++ examples/example.create.and.delete.person.php
@@ -3,9 +3,10 @@ require_once '../vendor/autoload.php';
 
 use Genkgo\Api\Connection;
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\HttpFactory;
 
 $config = require_once 'config.php';
-$connection = new Connection(new Client(), $config['url'], $config['token']);
+$connection = new Connection(new Client(), new HttpFactory(), $config['url'], $config['token']);
 
 $folder = $connection->command('organization', 'find', [
     'name' => 'Aanmeldingen'
--- examples/example.login.php
+++ examples/example.login.php
@@ -3,9 +3,10 @@ require_once '../vendor/autoload.php';
 
 use Genkgo\Api\Connection;
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\HttpFactory;
 
 $config = require_once 'config.php';
-$connection = new Connection(new Client(), $config['url'], $config['token']);
+$connection = new Connection(new Client(), new HttpFactory(), $config['url'], $config['token']);
 
 $result = $connection->command('organization', 'login', [
     'uid' => 'test',
